bcc92cc3cb0008c949e0add707b38e08772d1b8b
9Volt9Volt <09volt@gmail.com>
2015-10-10 17:37:38 -0400

    added euclidean distance

--- pca_final.py
+++ pca_final.py
@@ -3,19 +3,20 @@ import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from matplotlib import pyplot as plt
+from scipy.spatial.distance import euclidean
 
 with open('pickled_data', 'rb') as read:
     all_data = pickle.load(read)
 all_samples = all_data['matrix'].T
 sample_ids = all_data['ids']
 
-def PCA2D(X):
+def PCA2D(X, ids):
     # Standardize
     X_std = StandardScaler().fit_transform(X.T)
     # PCA
     sklearn_pca = PCA(n_components=2)
     X_transf = sklearn_pca.fit_transform(X_std)
-    return X_transf
+    return combine(X_transf, ids)
 
 def combine(X, ids, filename=None):
     all_data = []
@@ -27,3 +28,11 @@ def combine(X, ids, filename=None):
         f = open(filename, 'w')
         f.write(str(all_data))
         f.close()
+
+def closest(X, point):
+    smallest = [euclidean(point, X[0][1]), X[0]]
+    distance = 0
+    for points in X[1:]:
+        distance = euclidean(point, points[1])
+        if distance < smallest[0]: smallest = [distance, points[0]]
+    return smallest
